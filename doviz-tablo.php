d86b334466d935c94c63b43325232bc37c42077b
Selim Altınkaynak <selim@hipmedya.com>
2026-09-02 18:42:45 +0300

    Remove parent links and flags from currency list

--- doviz-tablo.php
+++ doviz-tablo.php
@@ -2,78 +2,70 @@
 /*
   Template Name: Döviz Tablo
 */
-// Child theme override: original BirFinans data/parsing logic preserved; scoped wrapper and modern market sidebar added for Piyasa Vizyon styling.
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+global $currency_data;
 
 get_header();
 ?>
 <style>
-    .currencyTable tr th {
-        font-weight: 500;
-    }
-
-    .currencyTable tr td b {
-        color: #3b72de !important;
-    }
+.pv-market-currency-native .currencyTable tr th{font-weight:500;}
+.pv-market-currency-native .currencyTable tr td b{color:#3b72de!important;}
+.pv-market-currency-native .pv-currency-code{display:inline-flex;align-items:center;justify-content:center;min-width:42px;height:26px;margin-right:8px;padding:0 9px;border-radius:999px;background:#eef4ff;color:#2057b8;font-size:11px;font-weight:800;vertical-align:middle;}
+.pv-market-currency-native .pv-currency-name-link{display:inline-flex;align-items:center;text-decoration:none;}
 </style>
-<!-- Site Wrapper -->
 <div class="site-wrapper pv-market-native pv-market-currency-native">
-
-    <!-- Content -->
     <section class="content home">
         <div class="container-wrap">
-
-            <!-- WideBar -->
             <div class="widebar floatLeft">
-
                 <div class="singleWrapper">
-
-                    <!-- BreadCrumb -->
                     <div class="breadcrumb">
                         <ul class="block">
-                            <li><a href="<?php bloginfo('home') ?>">Anasayfa<i>/</i></a></li>
-                            <li class="post bg"><span><?php the_title() ?></span></li>
+                            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Anasayfa<i>/</i></a></li>
+                            <li class="post bg"><span><?php the_title(); ?></span></li>
                         </ul>
                     </div>
 
-                    <h1 class="postTitle"><?php the_title() ?></h1>
-
-
+                    <h1 class="postTitle"><?php the_title(); ?></h1>
 
                     <div class="pv-market-list-content pv-currency-list-content">
-
-                        <!-- Main Content -->
                         <div class="mainContent">
-
-                            <!-- Main -->
                             <div class="main">
-
-                                <!-- widget -->
                                 <div class="widget">
-                                    <!-- Currency Showcase -->
                                     <div class="currencyShowcase fullShowcase mobileBottomNo">
-                                        <?php if (wp_is_mobile()) {
-    ?>
+                                        <?php if ( wp_is_mobile() ) : ?>
                                             <table class="currencyTable currencyFullTable">
                                                 <tr>
-                                                    <th style="width: 70% !important;display: inline-block;">Döviz</th>
-                                                    <th class="sagagit2" style="width: 20%;display: inline-block;padding-left: 0;">Alış</th>
+                                                    <th style="width:70%!important;display:inline-block;">Döviz</th>
+                                                    <th class="sagagit2" style="width:20%;display:inline-block;padding-left:0;">Alış</th>
                                                 </tr>
-                                                <?php foreach (array_unique($currency_data['code']) as $key => $val) :
-                                                    if (str_replace(",", ".", $currency_data['change_rate'][$key]) > 0) {
-                                                        $crease_status = "increase";
-                                                    } else {
-                                                        $crease_status = "decrease";
-                                                    } ?>
-                                                    <tr class="alt dKurlariS">
-                                                        <td style="width: 70% !important;display: inline-block;"><a href="<?php bloginfo("home") ?>/<?= $bp_options['page_doviz'] ?>?c=<?= $key ?>"><img src="<?php bloginfo('template_directory'); ?>/img/flag/<?= str_replace("csk", "czk", $key) ?>.png" width="24" height="16" alt="<?= $currency_data['full_name'][$key] ?> - <?= strtoupper($currency_data['code'][$key]) ?>"> <b><?= strtoupper($currency_data['code'][$key]) ?></b></a></td>
-                                                        <td style="width: 20%;display: inline-block;padding-left:0px;"><i class="<?= $crease_status ?>" style="<?php if (!is_user_logged_in()) : ?>position: relative; top: 26px;<?php endif; ?>"></i> <span><?= $currency_data['selling'][$key] ?></span></td>
-                                                    </tr>
-                                                <?php endforeach; ?>
-                                            </table>
-
-                                        <?php
-} else {
+                                                <?php if ( is_array( $currency_data ) && ! empty( $currency_data['code'] ) ) : ?>
+                                                    <?php foreach ( array_unique( $currency_data['code'] ) as $key => $val ) : ?>
+                                                        <?php
+                                                        if ( ! isset( $currency_data['full_name'][ $key ], $currency_data['selling'][ $key ] ) ) {
+                                                            continue;
+                                                        }
+                                                        $change        = isset( $currency_data['change_rate'][ $key ] ) ? (string) $currency_data['change_rate'][ $key ] : '0';
+                                                        $crease_status = (float) str_replace( ',', '.', $change ) > 0 ? 'increase' : 'decrease';
+                                                        $code          = strtoupper( (string) $currency_data['code'][ $key ] );
+                                                        $detail_url    = pv_market_currency_detail_url( $key );
                                                         ?>
+                                                        <tr class="alt dKurlariS">
+                                                            <td style="width:70%!important;display:inline-block;">
+                                                                <a class="pv-currency-name-link" href="<?php echo esc_url( $detail_url ); ?>">
+                                                                    <span class="pv-currency-code"><?php echo esc_html( $code ); ?></span>
+                                                                    <b><?php echo esc_html( $code ); ?></b>
+                                                                </a>
+                                                            </td>
+                                                            <td style="width:20%;display:inline-block;padding-left:0;">
+                                                                <i class="<?php echo esc_attr( $crease_status ); ?>" style="<?php if ( ! is_user_logged_in() ) : ?>position:relative;top:26px;<?php endif; ?>"></i>
+                                                                <span><?php echo esc_html( $currency_data['selling'][ $key ] ); ?></span>
+                                                            </td>
+                                                        </tr>
+                                                    <?php endforeach; ?>
+                                                <?php endif; ?>
+                                            </table>
+                                        <?php else : ?>
                                             <table class="currencyTable currencyFullTable">
                                                 <tr>
                                                     <th>Döviz</th>
@@ -82,50 +74,50 @@ get_header();
                                                     <th>Fark</th>
                                                     <th>Saat</th>
                                                 </tr>
-                                                <?php foreach (array_unique($currency_data['code']) as $key => $val) :
-                                                    if (str_replace(",", ".", $currency_data['change_rate'][$key]) > 0) {
-                                                        $crease_status = "increase";
-                                                        $color          = "#40bc9a";
-                                                    } else {
-                                                        $crease_status = "decrease";
-                                                        $color          = "#fc4b67";
-                                                    } ?>
-                                                    <tr>
-                                                        <td><a href="<?php bloginfo("home") ?>/<?= $bp_options['page_doviz'] ?>?c=<?= $key ?>"><img src="<?php bloginfo('template_directory'); ?>/img/flag/<?= str_replace("csk", "czk", $key) ?>.png" width="24" height="16" alt="<?= $currency_data['full_name'][$key] ?> - <?= strtoupper($currency_data['code'][$key]) ?>"> <b><?= $currency_data['full_name'][$key] ?> - <?= strtoupper($currency_data['code'][$key]) ?></b></a></td>
-                                                        <td style="font-weight: 500;color:<?= $color ?>;"><i class="<?= $crease_status ?>"></i> <?= $currency_data['selling'][$key] ?></td>
-                                                        <td style="font-weight: normal;"><?= $currency_data['buying'][$key] ?></td>
-                                                        <td style="font-weight: normal;"><span class="<?= $crease_status ?> subtract">% <?= $currency_data['change_rate'][$key] ?></span></td>
-                                                        <td style="padding: 0 15px;font-weight: normal;"><?= $currency_data['time'][$key] ?></td>
-                                                    </tr>
-                                                <?php endforeach; ?>
+                                                <?php if ( is_array( $currency_data ) && ! empty( $currency_data['code'] ) ) : ?>
+                                                    <?php foreach ( array_unique( $currency_data['code'] ) as $key => $val ) : ?>
+                                                        <?php
+                                                        if ( ! isset( $currency_data['full_name'][ $key ], $currency_data['selling'][ $key ], $currency_data['buying'][ $key ] ) ) {
+                                                            continue;
+                                                        }
+                                                        $change        = isset( $currency_data['change_rate'][ $key ] ) ? (string) $currency_data['change_rate'][ $key ] : '0';
+                                                        $is_up         = (float) str_replace( ',', '.', $change ) > 0;
+                                                        $crease_status = $is_up ? 'increase' : 'decrease';
+                                                        $color         = $is_up ? '#40bc9a' : '#fc4b67';
+                                                        $code          = strtoupper( (string) $currency_data['code'][ $key ] );
+                                                        $name          = (string) $currency_data['full_name'][ $key ];
+                                                        $time          = isset( $currency_data['time'][ $key ] ) ? (string) $currency_data['time'][ $key ] : '';
+                                                        $detail_url    = pv_market_currency_detail_url( $key );
+                                                        ?>
+                                                        <tr>
+                                                            <td>
+                                                                <a class="pv-currency-name-link" href="<?php echo esc_url( $detail_url ); ?>">
+                                                                    <span class="pv-currency-code"><?php echo esc_html( $code ); ?></span>
+                                                                    <b><?php echo esc_html( $name . ' - ' . $code ); ?></b>
+                                                                </a>
+                                                            </td>
+                                                            <td style="font-weight:500;color:<?php echo esc_attr( $color ); ?>;"><i class="<?php echo esc_attr( $crease_status ); ?>"></i> <?php echo esc_html( $currency_data['selling'][ $key ] ); ?></td>
+                                                            <td style="font-weight:normal;"><?php echo esc_html( $currency_data['buying'][ $key ] ); ?></td>
+                                                            <td style="font-weight:normal;"><span class="<?php echo esc_attr( $crease_status ); ?> subtract">% <?php echo esc_html( $change ); ?></span></td>
+                                                            <td style="padding:0 15px;font-weight:normal;"><?php echo esc_html( $time ); ?></td>
+                                                        </tr>
+                                                    <?php endforeach; ?>
+                                                <?php endif; ?>
                                             </table>
-                                        <?php
-                                                    } ?>
+                                        <?php endif; ?>
                                     </div>
-                                    <!-- //Currency Showcase -->
                                 </div>
-                                <!-- //widget -->
-
                             </div>
-
-
                         </div>
-                        <!-- #MainBar -->
-
-
                     </div>
-
                 </div>
-
             </div>
-            <?php if (!wp_is_mobile() && function_exists('pv_v7_market_sidebar')) {
-                pv_v7_market_sidebar('doviz-kurlari');
+
+            <?php if ( ! wp_is_mobile() && function_exists( 'pv_v7_market_sidebar' ) ) {
+                pv_v7_market_sidebar( 'doviz-kurlari' );
             } ?>
         </div>
     </section>
-    <!-- Content -->
     <div class="clear"></div>
-
 </div>
-<!-- #Site Wrapper -->
 <?php get_footer(); ?>
