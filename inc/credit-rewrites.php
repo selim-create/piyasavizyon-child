d632b23605a7e6c9a093d54c85f76b9809fd2165
Selim Altınkaynak <selim@hipmedya.com>
2026-09-02 20:20:08 +0300

    Persist child-owned credit rewrite rules

--- inc/credit-rewrites.php
+++ inc/credit-rewrites.php
@@ -1,26 +1,37 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-function pv_credit_register_pretty_rewrites() {
-    $routes = array(
-        'ihtiyac-kredisi' => 'ihtiyac-kredisi',
-        'konut-kredisi'   => 'konut-kredisi',
-        'tasit-kredisi'   => 'tasit-kredisi',
-        'kobi-kredisi'    => 'kobi-kredisi',
+function pv_credit_pretty_rule_map() {
+    return array(
+        'ihtiyac-kredisi/([0-9]+)-ay-([0-9]+)-tl-kredi/?$' => 'index.php?pagename=ihtiyac-kredisi',
+        'konut-kredisi/([0-9]+)-ay-([0-9]+)-tl-kredi/?$'   => 'index.php?pagename=konut-kredisi',
+        'tasit-kredisi/([0-9]+)-ay-([0-9]+)-tl-kredi/?$'   => 'index.php?pagename=tasit-kredisi',
+        'kobi-kredisi/([0-9]+)-ay-([0-9]+)-tl-kredi/?$'    => 'index.php?pagename=kobi-kredisi',
     );
+}
 
-    foreach ( $routes as $prefix => $page_slug ) {
-        add_rewrite_rule(
-            '^' . preg_quote( $prefix, '~' ) . '/([0-9]+)-ay-([0-9]+)-tl-kredi/?$',
-            'index.php?pagename=' . $page_slug,
-            'top'
-        );
+function pv_credit_register_pretty_rewrites() {
+    foreach ( pv_credit_pretty_rule_map() as $regex => $target ) {
+        add_rewrite_rule( $regex, $target, 'top' );
     }
 }
 add_action( 'init', 'pv_credit_register_pretty_rewrites', 20 );
 
+/**
+ * Keep the child-owned numeric credit rules in the persisted rewrite array even
+ * when the legacy parent theme injects broader credit patterns later.
+ */
+function pv_credit_prepend_persisted_pretty_rewrites( $rules ) {
+    if ( ! is_array( $rules ) ) {
+        $rules = array();
+    }
+
+    return pv_credit_pretty_rule_map() + $rules;
+}
+add_filter( 'rewrite_rules_array', 'pv_credit_prepend_persisted_pretty_rewrites', PHP_INT_MAX );
+
 function pv_credit_maybe_flush_pretty_rewrites() {
-    $version = '1';
+    $version = '2';
     if ( get_option( 'pv_credit_rewrite_version' ) === $version ) {
         return;
     }
